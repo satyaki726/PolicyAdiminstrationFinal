3abe5fa9089ab0437d6b430b83e3385ee7769694
satyaki726 <satyakisaha726@gmail.com>
2021-06-13 21:30:27 +0530

    First Commit

---
+++ PolicyAdministration/Authorization-Microservices/src/main/java/com/cts/authorization/AuthorizatiionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cts.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizatiionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizatiionMicroserviceApplication.class, args);
+	}
+
+}
